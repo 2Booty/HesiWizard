8298d928d9d46518239ac5ce67b8a9c01393332f
2Booty <58183813+2Booty@users.noreply.github.com>
2023-11-29 09:52:13 -0700

    Initial Release

---
+++ MessageBox.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hesi_Wizard
+{
+    /// <summary>
+    /// Interaction logic for MessageBox.xaml
+    /// </summary>
+    public partial class MessageBox : Window
+    {
+        internal string Path1;
+
+        public MessageBox()
+        {
+            InitializeComponent();
+        }
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
